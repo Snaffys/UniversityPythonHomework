1b459cdec9b0ebe47ae4838b429cd2f26bd3a384
Dan <grezindanil@gmail.com>
2025-10-12 20:54:29 +0300

    Add brute-force algorithm to count valid queens positions in chess

---
+++ HW4/chessQueenPosition/src/brute_force.py
@@ -0,0 +1,34 @@
+import itertools
+
+
+def check_diagonal_attacks(queen_positions):
+    for i in range(len(queen_positions) - 1):
+        for j in range(i + 1, len(queen_positions)):
+            # Diagonal attack occurs when |row1 - row2| == |col1 - col2|
+            if abs(queen_positions[i][0] - queen_positions[j][0]) == abs(queen_positions[i][1] - queen_positions[j][1]):
+                return False
+    return True
+
+
+def brute_force_n_queens(n):
+    solutions_count = 0
+    # Generate all collumn non-repeating permutations (queens will be in different rows anyway)
+    all_col_permutations = itertools.permutations(range(n))
+
+    for col_positions in all_col_permutations:
+        # Assign rows to each column and put results in the list
+        queen_positions = list(enumerate(col_positions))
+        if check_diagonal_attacks(queen_positions):
+            solutions_count += 1
+
+    return solutions_count
+
+
+def main():
+    BOARD_SIZE = 8
+    print("result:")
+    print(brute_force_n_queens(BOARD_SIZE))
+
+
+if __name__ == "__main__":
+    main()
