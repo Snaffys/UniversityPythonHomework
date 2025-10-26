82c1a51361b15ae7b871874de6624e73a872eba6
Dan <grezindanil@gmail.com>
2025-10-27 00:57:03 +0300

    Add python sort and quick sort for property based tests

---
+++ HW5/heapSort/src/sorts.py
@@ -0,0 +1,27 @@
+def python_sorted(arr):
+    return sorted(arr)
+
+
+def set_pivot(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[high], arr[i + 1] = arr[i + 1], arr[high]
+    return i + 1
+
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+
+    return quick_sort(left) + middle + quick_sort(right)
